eaec7b676b1b3b8fac80f9fd590e91241606b23b
Rahul <rahulpoonia229@gmail.com>
2025-07-12 12:31:52 +0530

    Cleanup

--- src/types/session.ts
+++ src/types/session.ts
@@ -1,15 +1,5 @@
 import z from "zod";
 
-// export const defaultAuthCredentials: AuthCredantials = {
-//     rollNo: process.env.ERP_USER || "23BT10029",
-//     password: process.env.ERP_PASS || "rahulerp",
-//     securityAnswers: {
-//         "1": "a",
-//         "2": "b",
-//         "3": "c",
-//     },
-// };
-
 export const AuthCredantialsSchema = z.object({
     rollNo: z.string().regex(/^\d{2}[A-Za-z]{2}\d{5}$/, {
         message: "rollNo must be in format DDYYDDDDD (e.g., 23XX10012)",
